5c7069b3c2cfd511fd4ffb02601b243b489c4e61
Thomas Mandorfer <t.mandorfer@gmail.com>
2025-01-06 01:16:36 +0100

    feat: update bootstrap button

---
+++ src/components/helpers/BootstrapButton.tsx
@@ -0,0 +1,20 @@
+import "../../style.css"
+
+export type ButtonProps = {
+    label?: string
+    class?: string
+    disabled?: boolean
+    id?: string
+    onClick?:(e: React.MouseEvent<HTMLButtonElement>) => void
+}
+export function BootstrapButton(props:ButtonProps){
+    return <><div className="custom-bootstrap"><button 
+    id={props?.id}  
+    onClick={props?.onClick} 
+    disabled={props?.disabled}
+    className={(props?.class ?? '') + ' btn btn-outline-primary'}>    
+        {props?.label}
+    </button>
+    </div>
+    </>
+}
