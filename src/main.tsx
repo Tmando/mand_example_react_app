09a37961b1d7a467959d5cc76aacd9e83c646a6f
Thomas Mandorfer <t.mandorfer@gmail.com>
2025-01-06 01:18:25 +0100

    feat: update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,6 @@
+import r2wc from "@r2wc/react-to-web-component"
+import { RandomNumberComponent } from "./components/RandomNumberComponent"
 
+const RandomNumberComponentWebComponent = r2wc(RandomNumberComponent)
+
+customElements.define("mando-random-number", RandomNumberComponentWebComponent)
