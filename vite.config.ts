62aaebde6ad1ee542b42c95f4ac8ec8ca75be252
Thomas Mandorfer <t.mandorfer@gmail.com>
2025-01-06 01:15:37 +0100

    feat: update vite file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import typescript from '@rollup/plugin-typescript'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  define: {
+    "process.env": {
+      NODE_ENV: "production",
+    },
+  },
+  plugins: [react(),typescript()],
+  build: {
+    lib: {
+      formats: ['es', 'umd'],
+      entry: './src/main.tsx',
+      name: 'mando-react-web-components',
+      fileName: 'mando-react-web-components'
+    }
+  },
 })
